b309443aef950613654294efc4bc951dab6806c8
George Berry <george.e.berry@gmail.com>
2017-10-02 22:54:34 -0700

    print

--- src/simulation/aggregate_runs.py
+++ src/simulation/aggregate_runs.py
@@ -1,5 +1,7 @@
 """
 Call from command line, point towards a folder
+
+python aggregate_runs.py -i /mnt/md0/network_sampling_data/sim_output_dd -o outfile.tsv
 """
 import click
 import glob
@@ -14,9 +16,11 @@ def agg(input_path: str, output_file: str):
 
     flist = glob.glob(input_path + '/*.p')
     for fname in flist:
+        click.echo('Parsing file {}'.format(fname))
         with open(fname, 'rb') as f:
             records = pickle.load(f)
             all_records.extend(records)
+
 
     df = pd.DataFrame(all_records)
     df.to_csv(output_file, sep='\t')
