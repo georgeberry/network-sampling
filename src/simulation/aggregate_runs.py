839fc90e9752a7b97e58fc1fe1cbf3621e8389dc
George Berry <george.e.berry@gmail.com>
2017-10-02 22:51:09 -0700

    aggregation code

---
+++ src/simulation/aggregate_runs.py
@@ -0,0 +1,24 @@
+"""
+Call from command line, point towards a folder
+"""
+import click
+import glob
+import pickle
+import pandas as pd
+
+@click.command()
+@click.option('--input-path', '-i')
+@click.option('--output_file', '-o')
+def agg(input_path: str, output_file: str):
+    all_records = []
+
+    flist = glob.glob(input_path + '/*.p')
+    for fname in flist:
+        with open(fname, 'rb') as f:
+            records = pickle.load(f)
+            all_records.extend(records)
+
+    return pd.DataFrame(all_records)
+
+if __name__ == '__main__':
+    agg()
